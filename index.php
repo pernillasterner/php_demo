83d889d15a97851770c6b1612bf287549599a9ca
Pernilla Sterner <psterner@hotmail.com>
2024-11-12 13:08:11 +0100

    Conditions

--- index.php
+++ index.php
@@ -1,16 +1,30 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Demo</title>
 </head>
+
 <body>
-    
-<h1><?php
 
-$greeting = "Hello";
-echo $greeting . " World"; ?></h1>
+    <?php
+    $name = "Dark Matter";
+    $read = false;
 
+    if ($read) {
+        $message = "You have read $name";
+    } else {
+        $message = "You have NOT read $name";
+    }
+    ?>
+
+    <h1>
+        <?php echo $message; ?>
+        <?= $message; ?>
+    </h1>
 </body>
+
 </html>
