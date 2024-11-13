8aff2519b159d9dc512ec15e35153e4206e29274
Pernilla Sterner <psterner@hotmail.com>
2024-11-13 13:43:20 +0100

    Add condition for hover effect on navbar

--- index.php
+++ index.php
@@ -16,12 +16,5 @@ function dd($value)
 
 // dd($_SERVER);
 
-echo $_SERVER['REQUEST_URI'];
-
-if ($_SERVER['REQUEST_URI']  === '/') {
-    echo 'bg-gray-900 text-white';
-} else {
-    echo 'text-gray-300';
-}
 
 require "views/index.view.php";
--- views/partials/nav.php
+++ views/partials/nav.php
@@ -8,13 +8,9 @@
                 <div class="hidden md:block">
                     <div class="ml-10 flex items-baseline space-x-4">
                         <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-                        <a href="/" class="<?php if ($_SERVER['REQUEST_URI']  === '/') {
-                                                echo 'bg-gray-900 text-white';
-                                            } else {
-                                                echo 'text-gray-300';
-                                            } ?>hover:bg-gray-700 hover:text-white px-3 rounded-md py-2 text-sm font-medium" aria-current="page">Home</a>
-                        <a href="/about.php" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">About</a>
-                        <a href="/contact.php" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Contact</a>
+                        <a href="/" class="<?= $_SERVER['REQUEST_URI'] === '/' ? 'bg-gray-900 text-white' : 'text-gray-300'; ?>rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white" aria-current="page">Home</a>
+                        <a href="/about.php" class="<?= $_SERVER['REQUEST_URI'] === '/about.php' ? 'bg-gray-900 text-white' : 'text-gray-300'; ?>rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">About</a>
+                        <a href="/contact.php" class="<?= $_SERVER['REQUEST_URI'] === '/contact.php' ? 'bg-gray-900 text-white' : 'text-gray-300'; ?>rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Contact</a>
                     </div>
                 </div>
             </div>
