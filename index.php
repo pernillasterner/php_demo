a75d290b06035134d8447b8b25456d6332e42728
Pernilla Sterner <psterner@hotmail.com>
2024-11-13 15:03:27 +0100

    Demo on how to use parse and seperate query from uri

--- index.php
+++ index.php
@@ -4,6 +4,19 @@ require 'functions.php';
 
 $uri = $_SERVER['REQUEST_URI'];
 
+// parse will separate the path from the query string
+
+dd(parse_url($uri));
+
+// Results
+// contact?name=johndoe
+// array(2) {
+//     ["path"]=>
+//     string(8) "/contact"
+//     ["query"]=>
+//     string(12) "name=johndoe"
+//   }
+
 if ($uri === '/') {
     require 'controllers/index.php';
 } else if ($uri === '/about') {
