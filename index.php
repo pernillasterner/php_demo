d765c8659b74e810c0951e4abeb6fc74d4687cdf
Pernilla Sterner <psterner@hotmail.com>
2024-11-13 13:21:11 +0100

    Create function to display server info. Clean up

--- index.php
+++ index.php
@@ -1,3 +1,27 @@
 <?php
 
+$heading = 'Home';
+
+
+
+
+function dd($value)
+{
+    echo "<pre>";
+    var_dump($value);
+    echo "<pre>";
+
+    die();
+}
+
+// dd($_SERVER);
+
+echo $_SERVER['REQUEST_URI'];
+
+if ($_SERVER['REQUEST_URI']  === '/') {
+    echo 'bg-gray-900 text-white';
+} else {
+    echo 'text-gray-300';
+}
+
 require "views/index.view.php";
--- views/partials/nav.php
+++ views/partials/nav.php
@@ -8,7 +8,11 @@
                 <div class="hidden md:block">
                     <div class="ml-10 flex items-baseline space-x-4">
                         <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-                        <a href="/" class=" px-3 py-2 text-sm font-medium text-white" aria-current="page">Home</a>
+                        <a href="/" class="<?php if ($_SERVER['REQUEST_URI']  === '/') {
+                                                echo 'bg-gray-900 text-white';
+                                            } else {
+                                                echo 'text-gray-300';
+                                            } ?>hover:bg-gray-700 hover:text-white px-3 rounded-md py-2 text-sm font-medium" aria-current="page">Home</a>
                         <a href="/about.php" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">About</a>
                         <a href="/contact.php" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Contact</a>
                     </div>
