094c7369e44380bfd6eb2bc2fef1fd572796cbae
Pernilla Sterner <psterner@hotmail.com>
2024-11-14 13:58:53 +0100

    Move class into seperate folder. Make config info more dynamic

---
+++ Database.php
@@ -0,0 +1,42 @@
+<?php
+
+// Connect to the database, and execute a query
+class Database
+{
+    public $connection;
+
+    // When and instance is constructed. First thing to run
+    public function __construct()
+    {
+        $config = [
+            'host' => 'localhost',
+            'port' => 3306,
+            'dbname' => 'myapp',
+            'charset' => 'utf8mb4'
+        ];
+
+
+
+
+        // Setup connection to the MySQL database using PDO (PHP Data Objects).
+        // Data Source Name (DNS) specifies the connection details for MySQL: Like a connection string
+        $dsn = "mysql:host={$config['host']};port={$config['port']};dbname={$config['dbname']};charset={$config['charset']}";
+
+        // Initialize the PDO instance to connect to the database
+        $this->connection = new PDO($dsn, 'root', '', [
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+        ]);
+    }
+
+    public function query($query)
+    {
+
+        $statement = $this->connection->prepare($query);
+
+        // Execute the code
+        $statement->execute();
+
+        // Fetch the results and remove duplicate array
+        return $statement;
+    }
+}
--- index.php
+++ index.php
@@ -1,37 +1,11 @@
 <?php
 
 require 'functions.php';
+require 'Database.php';
 // require 'router.php';
 
 
-// Connect to the database, and execute a query
-class Database
-{
-    public $connection;
 
-    // When and instance is constructed. First thing to run
-    public function __construct()
-    {
-        // Setup connection to the MySQL database using PDO (PHP Data Objects).
-        // Data Source Name (DNS) specifies the connection details for MySQL: Like a connection string
-        $dns = "mysql:host=localhost;port=3306;dbname=myapp;charset=utf8mb4";
-
-        // Initialize the PDO instance to connect to the database
-        $this->connection = new PDO($dns, 'root');
-    }
-
-    public function query($query)
-    {
-
-        $statement = $this->connection->prepare($query);
-
-        // Execute the code
-        $statement->execute();
-
-        // Fetch the results and remove duplicate array
-        return $statement;
-    }
-}
 
 // Initialize a new instance of a class
 $db = new Database();
