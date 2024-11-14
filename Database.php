0962a74330e4e962df7bc927751b58645392e70e
Pernilla Sterner <psterner@hotmail.com>
2024-11-14 14:30:49 +0100

    Put config data in seperate folder. Clean up code

--- Database.php
+++ Database.php
@@ -6,21 +6,13 @@ class Database
     public $connection;
 
     // When and instance is constructed. First thing to run
-    public function __construct()
+    public function __construct($config)
     {
-        $config = [
-            'host' => 'localhost',
-            'port' => 3306,
-            'dbname' => 'myapp',
-            'charset' => 'utf8mb4'
-        ];
-
-        // http_build_query($data); // exemple.com?host=localhost&port=3306&dbname=myapp
-        $dsn = 'mysql:' . http_build_query($config, '', ';'); // host=localhost;port=3306;dbname=myapp
 
         // Setup connection to the MySQL database using PDO (PHP Data Objects).
         // Data Source Name (DNS) specifies the connection details for MySQL: Like a connection string
-        // $dsn = "mysql:host={$config['host']};port={$config['port']};dbname={$config['dbname']};charset={$config['charset']}";
+        $dsn = 'mysql:' . http_build_query($config, '', ';'); // host=localhost;port=3306;dbname=myapp
+
 
         // Initialize the PDO instance to connect to the database
         $this->connection = new PDO($dsn, 'root', '', [
--- index.php
+++ index.php
@@ -1,14 +1,16 @@
 <?php
 
 require 'functions.php';
-require 'Database.php';
 // require 'router.php';
+require 'Database.php';
 
 
+// Config data
+$config = require('config.php');
 
 
 // Initialize a new instance of a class
-$db = new Database();
+$db = new Database($config);
 
 // Calling the query method
 $posts = $db->query("select * from posts")->fetchAll(PDO::FETCH_ASSOC);
