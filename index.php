4bad82078c0e6ead559128fc90ba5f77c1968a6b
Pernilla Sterner <psterner@hotmail.com>
2024-11-14 09:34:10 +0100

    Clean up code. Declare function to handle routes

--- index.php
+++ index.php
@@ -4,28 +4,33 @@ require 'functions.php';
 
 $uri = parse_url($_SERVER['REQUEST_URI'])['path'];
 
-
-// if ($uri === '/') {
-//     require 'controllers/index.php';
-// } else if ($uri === '/about') {
-//     require 'controllers/about.php';
-// } else if ($uri === '/contact') {
-//     require 'controllers/contact.php';
-// }
-
-// Associative array
+// Declaration of routes
 $routes = [
     '/' => 'controllers/index.php',
     '/about' => 'controllers/about.php',
     '/contact' => 'controllers/contact.php',
 ];
 
-if (array_key_exists($uri, $routes)) {
-    require $routes[$uri];
-} else {
-    http_response_code(404);
 
-    require 'views/404.php';
+// Function to handle routing
+function routeToController($uri, $routes)
+{
+    if (array_key_exists($uri, $routes)) {
+        require $routes[$uri];
+    } else {
+        abort();
+    }
+}
+
+// Function that will abort when given a status code
+function abort($code = 404)
+{
+    http_response_code($code);
+
+    require "views/{$code}.php";
 
     die();
 }
+
+
+routeToController($uri, $routes);
